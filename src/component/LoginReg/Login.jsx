95e0427007492ad650583fa51abd645b6c56d835
bezbrain <belloquadriolawale@gmail.com>
2023-07-18 12:43:12 -0700

    Imported eyes and circled-times icons in the login inputs

--- src/component/LoginReg/Login.jsx
+++ src/component/LoginReg/Login.jsx
@@ -1,3 +1,4 @@
+import { FaEye, FaTimesCircle } from "react-icons/fa";
 import Logo from "../Logo";
 import styles from "../../styles/login-reg-styles/login.module.css";
 import { useGlobalContent } from "../../context";
@@ -11,10 +12,16 @@ const Login = () => {
         <form>
           <Logo />
           <p>Log in or sign up</p>
-          <div className={styles.inputCon}>
+          <div className={styles.inputEmail}>
+            <div>
+              <FaTimesCircle className={styles.eraseText} />
+            </div>
             <input type="text" placeholder="Enter email or username" />
           </div>
-          <div className={styles.inputCon}>
+          <div className={styles.inputPass}>
+            <div>
+              <FaEye className={styles.togglePass} />
+            </div>
             <input type="password" placeholder="Enter password" />
           </div>
           <button>Sign in</button>
